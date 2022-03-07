98ee8068c72eff0975349f3e35d07df717e81b3e
Mihai Morariu <mihaimorariu@gmail.com>
2022-03-07 22:07:04 +0200

    Get the number of unique labels and aminos from SequenceDataset.

--- protclf/datasets/sequence.py
+++ protclf/datasets/sequence.py
@@ -32,10 +32,10 @@ class SequenceDataset(Dataset):
 
         self.max_seq_len = max_seq_len
         self.sequences = all_data["sequence"]
-        self.families = all_data["family_accession"]
+        self.labels = all_data["family_accession"]
 
-        self.word2id = self.__create_word2id_dict(self.sequences)
-        self.fam2lab = self.__create_fam2lab_dict(self.families)
+        self.amino2id = self.__create_amino2id_dict(self.sequences)
+        self.label2id = self.__create_label2id_dict(self.labels)
 
     def __load_csv(self,
                    data_dir: str,
@@ -53,16 +53,22 @@ class SequenceDataset(Dataset):
         all_data = pd.concat(all_data)
         return all_data
 
-    def __create_fam2lab_dict(self, families: Series) -> Dict[str, int]:
-        # Maps a family (as str) to a label (as int) and returns the
-        # corresponding dictionary.
-        unique_families = families.unique()
-        labels = {f: i for i, f in enumerate(unique_families, start=1)}
+    def get_num_unique_labels(self) -> int:
+        return len(self.label2id)
+
+    def get_num_unique_aminos(self) -> int:
+        return len(self.amino2id)
+
+    def __create_label2id_dict(self, labels: Series) -> Dict[str, int]:
+        # Maps a (family) label to an integer and returns the corresponding
+        # dictionary.
+        unique_labels = labels.unique()
+        labels = {f: i for i, f in enumerate(unique_labels, start=1)}
         labels["<unk>"] = 0
 
         return labels
 
-    def __create_word2id_dict(self, sequences):
+    def __create_amino2id_dict(self, sequences):
         # Maps a sequence (as str) to an ID (as int) and returns the
         # corresponding dictionary.
         vocabulary = set()
@@ -81,29 +87,29 @@ class SequenceDataset(Dataset):
     def __len__(self) -> int:
         return len(self.sequences)
 
-    def __convert_to_one_hot(self, text: str) -> torch.Tensor:
+    def __convert_to_one_hot(self, sequence: str) -> torch.Tensor:
         # Converts a given amino acid sequence to a one-hot encoding. The
-        # output is a torch.Tensor of shape [num_classes, n_samples].
-        sequence = []
+        # output is a torch.Tensor of shape [num_labels, n_samples].
+        amino_ids = []
 
-        for word in text[:self.max_seq_len]:
-            sequence.append(self.word2id.get(word, self.word2id['<unk>']))
+        for amino in sequence[:self.max_seq_len]:
+            amino_ids.append(self.amino2id.get(amino, self.amino2id['<unk>']))
 
-        if len(sequence) < self.max_seq_len:
-            remaining = self.max_seq_len - len(sequence)
-            sequence += [self.word2id['<pad>'] for _ in range(remaining)]
+        if len(amino_ids) < self.max_seq_len:
+            remaining = self.max_seq_len - len(amino_ids)
+            amino_ids += [self.amino2id['<pad>'] for _ in range(remaining)]
 
-        sequence = torch.as_tensor(sequence)
-        sequence_one_hot = one_hot(sequence, num_classes=len(self.word2id))
-        sequence_one_hot = sequence_one_hot.permute(1, 0)
+        amino_ids = torch.as_tensor(amino_ids)
+        amino_ids_one_hot = one_hot(amino_ids, num_classes=len(self.amino2id))
+        amino_ids_one_hot = amino_ids_one_hot.permute(1, 0)
 
-        return sequence_one_hot
+        return amino_ids_one_hot
 
     def __getitem__(self, index: int) -> Dict[str, torch.Tensor]:
-        sequence_one_hot = self.__convert_to_one_hot(
+        amino_ids_one_hot = self.__convert_to_one_hot(
             self.sequences.iloc[index])
-        label = self.fam2lab.get(self.families.iloc[index],
-                                 self.fam2lab['<unk>'])
-        label = torch.as_tensor(label)
+        label_id = self.label2id.get(self.labels.iloc[index],
+                                     self.label2id['<unk>'])
+        label_id = torch.as_tensor(label_id)
 
-        return {"sequence": sequence_one_hot, "label": label}
+        return {"sequence": amino_ids_one_hot, "label": label_id}
