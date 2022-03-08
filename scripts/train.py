6bb693e5b3e1f21a61e0a04ed482826e53f6f023
Mihai Morariu <mihaimorariu@gmail.com>
2022-03-08 17:54:27 +0200

    Make the training script compatible with PyTorch Lightning.

--- scripts/train.py
+++ scripts/train.py
@@ -12,24 +12,31 @@ def main(args):
     train_loader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=args.batch_size,
                                                shuffle=True,
-                                               num_workers=8)
+                                               num_workers=0)
 
     dev_dataset = SequenceDataset(args.data_dir, "dev")
     dev_loader = torch.utils.data.DataLoader(dev_dataset,
                                              batch_size=args.batch_size,
                                              shuffle=False,
-                                             num_workers=8)
+                                             num_workers=0)
 
-    model = ProtCNN(num_classes=train_dataset.get_num_unique_labels())
+    model = ProtCNN(
+        num_classes=train_dataset.get_num_unique_labels(),
+        learning_rate=args.learning_rate,
+        weight_decay=args.weight_decay,
+        momentum=args.momentum,
+        gamma=args.gamma,
+        milestones=args.milestones
+    )
     model.train()
 
-    trainer = pl.Trainer(gpus=args.gpus, max_epochs=args.max_epochs)
+    trainer = pl.Trainer.from_argparse_args(args)
     trainer.fit(model, train_loader, dev_loader)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", "-i", required=True)
+    parser.add_argument("--data_dir", "-d", required=True)
     parser.add_argument("--batch_size", "-b", type=int, default=512)
     parser.add_argument("--seed", "-s", type=int, default=0)
 
